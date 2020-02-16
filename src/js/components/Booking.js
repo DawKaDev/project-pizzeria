f3ffc705f23c18d61f459e7024f1bb5abad490a4
Dawid Krystochowicz <daw_kryst@interia.pl>
2020-02-16 11:45:29 +0100

    add date and hour picker

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,8 @@
 import {select, templates} from '../settings.js';
 import {utils} from '../utils.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 class Booking {
   constructor(element){
     const thisBooking = this;
@@ -13,6 +15,8 @@ class Booking {
     thisBooking.dom = {};
     thisBooking.dom.wrapper = element;
     thisBooking.dom.wrapper = utils.createDOMFromHTML(generatedHTML);
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
     const bookingContainer = document.querySelector(select.containerOf.booking);
@@ -20,6 +24,8 @@ class Booking {
   }
   initWidgets(){
     const thisBooking = this;
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
   }
---
+++ src/js/components/HourPicker.js
@@ -0,0 +1,37 @@
+/*eslint no-undef: "error"*/
+import {utils} from '../utils.js';
+import {settings, select} from '../settings.js';
+import BaseWidget from './BaseWidget.js';
+//import rangeSlider from 'rangeSlider';
+
+class HourPicker extends BaseWidget{
+  constructor(wrapper){
+    super(wrapper, settings.hours.open);
+    const thisWidget = this;
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
+    thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
+    //console.log(thisWidget);
+    thisWidget.initPlugin();
+    //console.log('test',thisWidget.dom.wrapper);
+    thisWidget.value = settings.hours.open;
+  }
+  initPlugin(){
+    const thisWidget = this;
+    //console.log(thisWidget.dom.input);
+    rangeSlider.create(thisWidget.dom.input, {});
+    thisWidget.dom.input.addEventListener('input', function(event){
+      thisWidget.value = event.target.value;
+    });
+  }
+  parseValue(value){
+    return utils.numberToHour(value);
+  }
+  isValid(){
+    return true;
+  }
+  renderValue(){
+    const thisWidget = this;
+    thisWidget.dom.output.innerHTML = thisWidget.value;
+  }
+}
+export default HourPicker;
